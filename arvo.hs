af89d743b0b494d69bc9163f016d451dc85e7d88
Cris <cris>
2013-10-06 14:37:35 -0300

    Base da arvove e funções basicas.

---
+++ arvo.hs
@@ -0,0 +1,32 @@
+data ArvBin a = Null
+     | No a (ArvBin a) (ArvBin a)
+
+somaA Null = 0
+somaA (No x esq dir) = x + (somaA esq) + (somaA dir)
+
+multiplicaA Null = 0
+multiplicaA (No x esq dir) = x * (somaA esq) * (somaA dir)
+
+elementoPreordem Null = []
+elementoPreordem (No x esq dir) = [x] ++ (elementoPreordem esq) ++ (elementoPreordem dir)
+
+elementoOrdem Null = []
+elementoOrdem (No x esq dir) =  (elementoOrdem esq) ++ [x] ++(elementoOrdem  dir)
+
+elementoPosordem Null = []
+elementoPosordem (No x esq dir) =  (elementoPosordem esq)  ++(elementoPosordem  dir)++ [x]
+
+
+arvg = (No 10 (No 5 (No 2 Null Null)(No 7 Null Null))(No 15 (No 14 Null Null)(No 19 Null Null)))
+
+arvb = (No 2 (No 1 Null Null) (No 3 Null Null))
+
+
+
+verificaAltura Null = 0
+verificaAltura (No x esq dir) = 1 + max  (verificaAltura esq) (verificaAltura dir)
+
+balanciada Null = True
+balanciada (No x esq dir) = ( abs ((verificaAltura esq)-(verificaAltura dir))) <= 1 && (balanciada esq) && (balanciada dir)
+
+busca f (No x esq dir) = f `elem` (elementoPreordem (No x esq dir))
